fb84ad7e1689e99c93dc35cbf1dd19843838fb66
paolo <paolo@LAPTOP-PFR3VSID>
2024-02-07 14:39:33 +0100

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Cerchio
+    {
+        public double raggio { get; set; }
+        public Cerchio(double raggio)
+        {
+            this.raggio = raggio;
+        }
+        public override string ToString()
+        {
+            return $"{raggio}";
+        }
+        public static Cerchio Parse (string input)
+        {
+            string[] dimension = input.Split(',');
+            if(dimension.Length==2 && double.TryParse(dimension[0],out double raggio))
+            {
+                return new Cerchio(raggio);
+            }
+            return null;
+        }
+        public double Area()
+        {
+            return raggio * raggio;
+        }
+        public double Perimetro()
+        {
+            return 2 * 3.14 * raggio;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio c = new Cerchio (5);
+            Cerchio r =Cerchio.Parse(c.ToString());
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
